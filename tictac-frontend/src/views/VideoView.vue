a01d534d3c09a687b7cbb1da46d7323b4b888919
Kihyun Ryu <khryu0610@gmail.com>
2023-11-16 17:51:50 +0900

    chore: 잡일처리

--- tictac-frontend/src/views/VideoView.vue
+++ tictac-frontend/src/views/VideoView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div></div>
+  <div>video detail</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//TODO: 땜빵한 거라 수정하거나 제거해야됨.
+if (!location.hash) {
+  location.hash = '#reload';
+  location.href = location.href;
+}
+/*
+ie 지원
+if (location.href.indexOf('#reload') == -1) location.href += '#reload';
+*/
+</script>
 
 <style scoped></style>
