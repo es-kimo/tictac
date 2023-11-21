802c2649daaba7a7684d0407263511e1d0fe6705
InSeungHwang <disparaitree@gmail.com>
2023-11-22 00:13:14 +0900

    chore: 마이페이지 영상 목록 가져오는 기능 구현중

--- tictac-frontend/src/views/UserView.vue
+++ tictac-frontend/src/views/UserView.vue
@@ -17,9 +17,14 @@
     <section>
       <h3 class="tit-video">동영상</h3>
       <ul class="list-video">
-        <!-- TODO: key 수정 -->
+        <!-- TODO: key 수정: videoList에서 코드 가져옴 -->
         <li v-for="video in currentContent" :key="video">
-          <VideoCard :video="video"> </VideoCard>
+          <!-- <VideoCard :video="video"> </VideoCard> -->
+          <VideoCard @video-hover="handleHover" v-for="video in currentContent" :video="video">
+        <template v-slot:outer>
+          <VideoInfo :content="video.content" :username="video.userId" />
+        </template>
+      </VideoCard>
         </li>
       </ul>
     </section>
@@ -32,13 +37,25 @@ import BigProfile from '@/components/profile/BigProfile.vue';
 import TabItem from '@/components/common/TabItem.vue';
 import VideoCard from '@/components/video/VideoCard.vue';
 import { useVideoStore } from '@/stores/video';
+import { useUserStore } from '@/stores/user';
+import { useRoute } from 'vue-router';
+import { type Video } from '@/stores/video';
 
 const videoStore = useVideoStore();
+const userStore = useUserStore();
+const route = useRoute();
 
 const user = {
-  username: sessionStorage.getItem('username'),
-  id: sessionStorage.getItem('userId')
+  username: ref<String>(),
+  id: route.params.userId.slice(1)
 };
+
+const getUsername = async () => {
+  // console.log(route.params.userId.slice(1));
+  await userStore.getUsername(route.params.userId.slice(1));
+  user.username.value = userStore.username;
+  console.log(user.username.value)
+}
 
 const getUploadList = async () => {
   await videoStore.getUploadList(user.id);
@@ -49,24 +66,31 @@ const getUploadList = async () => {
 
 const getMyBookmarkList = async () => {
   await videoStore.getMyBookmarkList(user.id);
-  console.log(videoStore.videoList);
-  console.log(tabs[1].content);
+  // console.log(videoStore.videoList);
+  // console.log(tabs[1].content);
   tabs[1].content = videoStore.videoList;
 };
 
 onMounted(() => {
-  // 1. 업로드한 동영상 통신
+  // 1. url에 적힌 userId에 대응하는 username 받음
+  getUsername();
+
+  // 2. 업로드한 동영상 통신
   getUploadList();
 
-  // 2. 찜한 동영상 통신
+  // 3. 찜한 동영상 통신
   getMyBookmarkList();
+
+  console.log(tabs[0])
 });
+
 
 // tab
 const currentId = ref(0);
+// TODO: content 타입 어떢하지
 const tabs = [
-  { id: 0, label: '동영상', content: [1, 2], offset: 0 }, //비디오 객체가 들어갈 곳
-  { id: 1, label: '즐겨찾기', content: [3], offset: 118 }
+  { id: 0, label: '동영상', content: Ref<Video[]>, offset: 0 }, //비디오 객체가 들어갈 곳
+  { id: 1, label: '즐겨찾기', content: [], offset: 118 }
 ];
 const currentContent = computed(() => tabs[currentId.value].content);
 
@@ -75,6 +99,20 @@ const barStyle = computed(() => ({
   width: currentWidth.value + 'px',
   transform: 'translateX(' + tabs[currentId.value].offset + 'px)'
 }));
+
+
+// videoList에서 코드 가져옴
+const handleHover = (showVideoRef: Ref<boolean>) => {
+  //해당 영상만 재생
+  showVideoRef.value = true;
+  queue.push(showVideoRef);
+  if (queue.length > 1) {
+    while (queue.length > 1) {
+      queue.shift()!.value = false;
+    }
+  }
+};
+
 </script>
 
 <style scoped>
